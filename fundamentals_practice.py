c5b52c5e9a6a1939b0edacea00178f88d6fc6035
Kelvin Phat <kelvinphat12@gmail.com>
2021-06-15 11:30:18 -0700

    variable testing

--- fundamentals_practice.py
+++ fundamentals_practice.py
@@ -1,1 +1,29 @@
 print("Hello World")
+#print hello world
+name = "LeBron"
+age = 35
+salary = 37.44
+plays_basketball = True
+jersey_number = "23"
+
+car_name = "Toyota"
+x = 50
+my_first_name = "John"
+
+print(name)
+type(name)
+print(age)
+type(age)
+print(salary)
+type(salary)
+print(plays_basketball)
+type(plays_basketball)
+print(jersey_number)
+type(jersey_number)
+print(car_name)
+type(car_name)
+print(x)
+type(x)
+print(my_first_name)
+type(my_first_name)
+
